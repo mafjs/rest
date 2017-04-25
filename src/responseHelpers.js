6f922ab1d0bf3c45a2fcb721101b143992913fc2
alekzonder <alekzonder@gmail.com>
2017-04-25 19:41:31 +0700

    new: add sendFindResult response helper

--- src/responseHelpers.js
+++ src/responseHelpers.js
@@ -97,6 +97,22 @@ module.exports = {
         };
 
         this.httpContextNext();
+    },
+
+    sendFindResult: function (result, requestQuery, api) {
+        var metadata = {
+            resultset: {
+                count: result.docs.length,
+                total: result.total,
+                limit: requestQuery.limit,
+                offset: requestQuery.offset
+            }
+        };
+
+        var docs = api.clearSystemFields(result.docs);
+
+        this.result(docs, metadata);
     }
+
     // ...
 };
