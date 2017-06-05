3b9dc548c4269a06e48bbd08c57c8a6e86968ebf
alekzonder <alekzonder@gmail.com>
2017-06-05 09:49:24 +0700

    chg: request helper min value

--- src/requestHelpers/limit.js
+++ src/requestHelpers/limit.js
@@ -7,9 +7,10 @@ module.exports = {
         options = options || {};
 
         var defaultValue = options.default || 20;
+        var min = options.min || 0;
         var max = options.max || 100;
 
-        return joi.number().min(1).max(max).default(defaultValue);
+        return joi.number().min(min).max(max).default(defaultValue);
     },
 
     get: function (obj) {
